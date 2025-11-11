de7dbbb21f94cddcd7df7f1f0e5d0494c02e68ba
Thomas Aspinall <thomasasp@live.com>
2025-11-11 23:55:21 +1100

    fixed: time delay

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 
 const int stepsPerRevolution = 2048;  // number of steps per revolution
 const int RevolutionsPerMinute = 17;  // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
-const int coolDown = 5000;            // how long before the door relocks automatically
-int unlocked = 0;
-int currentTime = millis();
+const unsigned long coolDown = 5000;            // how long before the door relocks automatically
+bool unlocked = false;
+unsigned long currentTime = millis();
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
@@ -21,20 +21,20 @@ void loop() {
 
   if (incoming == 'u' && !unlocked) {
     myStepper.step(stepsPerRevolution);
-    unlocked = 1;
+    unlocked = true;
     Serial.println("Unlocked");
     currentTime = millis();
   }
 
   if (incoming == 'l' && unlocked) {
     myStepper.step(-stepsPerRevolution);
-    unlocked = 0;
+    unlocked = false;
     Serial.println("Locked");
   }
 
   if (unlocked && (millis() - currentTime > coolDown)) {
     myStepper.step(-stepsPerRevolution);
-    unlocked = 0;
+    unlocked = false;
     Serial.println("Locked");
   }
 }
